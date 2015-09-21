820bbbc779ad28f78d224310ad82b46002fb41e0
Jos van Bakel <josvanbakel@gmail.com>
2015-09-21 10:48:24 +0200

    Use hiera_hash to merge multiple layers of hiera

--- manifests/server/hostkeys.pp
+++ manifests/server/hostkeys.pp
@@ -1,3 +1,13 @@
 class ssh::server::hostkeys($hostkeys = {}) {
-  create_resources('::ssh::server::hostkey', $hostkeys)
+  
+  # Merge hashes from multiple layer of hierarchy in hiera
+  $hiera_hostkeys = hiera_hash("${module_name}::server::hostkeys", undef)
+
+  $fin_hostkeys = $hiera_hostkeys ? {
+    undef   => $hostkeys,
+    ''      => $hostkeys,
+    default => $hiera_hostkeys,
+  }
+
+  create_resources('::ssh::server::hostkey', $fin_hostkeys)
 }
